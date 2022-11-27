7ea1f300c544d33728e1b08fe2b5f08aebda3901
root <root@tiago-castro>
2022-11-27 18:01:35 -0300

    :tada: first commit

---
+++ src/Setup/index.ts
@@ -0,0 +1,16 @@
+import Server from "./Server";
+
+class Setup {
+    private server: Server
+
+    constructor () {
+		this.server = new Server()
+	}
+
+    boot () {
+        this.server.start()
+    }
+
+}
+
+export default Setup
